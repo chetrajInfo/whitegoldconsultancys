deddba4a4fddb8b7d76fe2c54eb4c3179340e38b
myinfo <chetraj0101@gmail.com>
2023-11-22 21:09:46 -0600

    added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderpageComponent } from './headerpage/headerpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderpageComponent
   ],
   imports: [
     BrowserModule,
